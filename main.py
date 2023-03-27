4f81ee34319795c34d2e75cff1c7277acc3c460d
Zvang2007 <833148@mystma.org>
2023-03-27 14:59:49 -0500

    2

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ def greaterThan(x,y):
     if x > y:
         return True
     else:
-        False
+     return False
 def lessThan(x,y):
     if x<y:
         return True
     else:
-        False
+       return False
 def equalTo(x,y):
     if x == y:
         return True
     else:
-        False
+       return False
 def greaterOrEqual(x,y):
     if x >= y:
         return True
     else:
-        False
+       return False
 def lessOrEqual(x,y):
     if x <= y:
         return True
     else:
-        False
+        return False
 
 greaterThan(6, 10) 
 equalTo(23, 23)
